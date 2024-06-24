fba064c6893a09701d3ab96b928bd8bc48eec320
usnimoj <jominsu0103@naver.com>
2024-06-25 00:54:30 +0900

    Feat : MypageService 주문내역 조회 기능 구현

--- src/main/java/com/github/supercodingproject2mall/mypage/service/MypageService.java
+++ src/main/java/com/github/supercodingproject2mall/mypage/service/MypageService.java
@@ -1,13 +1,18 @@
 package com.github.supercodingproject2mall.mypage.service;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.supercodingproject2mall.auth.repository.UserRepository;
 import com.github.supercodingproject2mall.cart.repository.CartRepository;
 import com.github.supercodingproject2mall.mypage.dto.MypageCartItemsDto;
 import com.github.supercodingproject2mall.mypage.dto.MypageUserInfo;
 import com.github.supercodingproject2mall.mypage.mapper.MypageMapper;
+import com.github.supercodingproject2mall.order.dto.OrderHistoryDto;
+import com.github.supercodingproject2mall.order.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -17,6 +22,8 @@ import java.util.stream.Collectors;
 public class MypageService {
     private final UserRepository userRepository;
     private final CartRepository cartRepository;
+    private final OrderRepository orderRepository;
+    private final ObjectMapper objectMapper;
 
     public Optional<MypageUserInfo> findUserInfo(Integer userId){
         return userRepository.findById(userId).map(MypageMapper.INSTANCE::userEntityToMypage);
@@ -27,5 +34,14 @@ public class MypageService {
         return results.stream()
                 .map(CartItemMapper.INSTANCE::toDto)
                 .collect(Collectors.toList());
+    }
+
+    public List<OrderHistoryDto> getOrderHistoryForUser(Integer userId) {
+        String json = orderRepository.findOrdersWithItemsByUserId(userId);
+        try {
+            return objectMapper.readValue(json, new TypeReference<List<OrderHistoryDto>>() {});
+        } catch (IOException e) {
+            throw new RuntimeException("JSON parsing error", e);
+        }
     }
 }
