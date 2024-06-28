4b348806f1eb10f2d2fdb5d301d9c80ef376d19c
usnimoj <jominsu0103@naver.com>
2024-06-28 13:13:39 +0900

    Feat : OrderItemDto 구현

---
+++ src/main/java/com/github/supercodingproject2mall/orderItem/dto/OrderItemDto.java
@@ -0,0 +1,18 @@
+package com.github.supercodingproject2mall.orderItem.dto;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class OrderItemDto {
+    private Integer itemId;
+    private Integer quantity;
+    private Integer pricePerUnit;
+    private String name;
+}
