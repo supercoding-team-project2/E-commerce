74037c7b00db1f1b0e2c1520cb8ee426b89bc5c1
usnimoj <jominsu0103@naver.com>
2024-06-28 13:11:11 +0900

    Feat : Option Value Repository 구현

---
+++ src/main/java/com/github/supercodingproject2mall/optionValue/repository/OptionValueRepository.java
@@ -0,0 +1,7 @@
+package com.github.supercodingproject2mall.optionValue.repository;
+
+import com.github.supercodingproject2mall.optionValue.entity.OptionValueEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OptionValueRepository extends JpaRepository<OptionValueEntity , Integer> {
+}
