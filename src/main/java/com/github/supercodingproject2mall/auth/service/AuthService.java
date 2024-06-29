5a381fc1c35f0d4ce4d1add5e791835176521107
Kim Chanho <beroso20th@gmail.com>
2024-06-30 03:55:35 +0900

    Refactor: 로그인시 상태코드 수정

--- src/main/java/com/github/supercodingproject2mall/auth/service/AuthService.java
+++ src/main/java/com/github/supercodingproject2mall/auth/service/AuthService.java
@@ -96,7 +96,7 @@ public class AuthService {
         List<CartItemEntity> cartItemList = cartItemRepository.findAllByCartId(cartId);
         int cartItemCount = cartItemList.size();
 
-        return ResponseEntity.status(HttpStatus.CREATED).body(new LoginResponse(ResponseType.SUCCESS.toString(), tokenDTO, cartItemCount));
+        return ResponseEntity.status(HttpStatus.OK).body(new LoginResponse(ResponseType.SUCCESS.toString(), tokenDTO, cartItemCount));
     }
 
     private Cookie createCookie(String key, String value) {
