76e4c98198db5e5de978079060cf619abe64f0cc
HyeYoung-Hwang <114129207+HyeYoung-Hwang@users.noreply.github.com>
2024-06-30 17:01:15 +0900

    Feat: 삭제할 장바구니 아이템이 유저의 장바구니 아이템이 맞는지 비교하기 위해 cartItemEntity를 가져오는 repository 구현

--- src/main/java/com/github/supercodingproject2mall/cartItem/repository/CartItemRepository.java
+++ src/main/java/com/github/supercodingproject2mall/cartItem/repository/CartItemRepository.java
@@ -2,6 +2,7 @@ package com.github.supercodingproject2mall.cartItem.repository;
 
 import com.github.supercodingproject2mall.cartItem.entity.CartItemEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,4 +10,7 @@ import java.util.List;
 @Repository
 public interface CartItemRepository extends JpaRepository<CartItemEntity,Integer> {
     List<CartItemEntity> findByCartId(Integer cartId);
+
+    @Query("SELECT i FROM CartItemEntity i WHERE i.id = :id")
+    CartItemEntity findCartIdById(Integer id);
 }
