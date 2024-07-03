004b0f1691867e8374afc2cab23933d0dde21f62
Kim Chanho <beroso20th@gmail.com>
2024-07-03 15:02:23 +0900

    Refactor: 토큰 유효시간 1시간으로 변경

--- src/main/java/com/github/supercodingproject2mall/auth/service/AuthService.java
+++ src/main/java/com/github/supercodingproject2mall/auth/service/AuthService.java
@@ -88,7 +88,7 @@ public class AuthService {
         UserEntity user = userRepository.findByEmail(loginDTO.getEmail())
                 .orElseThrow(() -> new UsernameNotFoundException("이메일에 해당하는 유저가 없습니다: " + loginDTO.getEmail()));
 
-        String accessToken = jwtTokenProvider.createToken("access", user.getEmail(), user.getId(), 600000L); // 10분
+        String accessToken = jwtTokenProvider.createToken("access", user.getEmail(), user.getId(), 600000L * 6); // 10분
         String refreshToken = jwtTokenProvider.createToken("refresh", user.getEmail(), user.getId(), 86400000L);
 
         addRefreshEntity(user.getEmail(), refreshToken, 86400000L);
