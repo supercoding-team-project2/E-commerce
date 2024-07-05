be906b61e8c1dd8230b27ff38836975f01bea3ff
Kim Chanho <beroso20th@gmail.com>
2024-07-05 13:04:35 +0900

    Refactor: https 수정

--- src/main/java/com/github/supercodingproject2mall/config/WebConfig.java
+++ src/main/java/com/github/supercodingproject2mall/config/WebConfig.java
@@ -12,8 +12,8 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOriginPatterns(
                         "http://localhost:3000/",
-                        "https://localhost:3000",
-                        "https://127.0.0.1:3000"
+                        "https://localhost:3000/",
+                        "https://127.0.0.1:3000/"
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
